d257a8b1d8033f9e34a93a0b348a028e41fc584e
Maharshi Shah <maharshishah@mail.usf.edu>
2020-05-17 20:12:08 +0530

    Added skills, stats and copyright sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
 $(document).ready(function() {
-	$('#slides').superslides();
+	
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000 //ms
+	});
+
+	var typed = new Typed(".typed",{
+		strings: ["Computer Engineer."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false	
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },	
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+    var skillsTopOffset = $(".skillsSection").offset().top;	
+    $(window).scroll(function()	{
+    	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+		    $('.chart').easyPieChart({
+		    //your options goes here
+				easing: 'easeInOut',
+				barColor: '#fff',
+				trackColor: false,
+				scaleColor: false,
+				lineWidth: 4,
+				size: 152,
+				onStep: function(from, to, percent)	{
+					$(this.el).find('.percent').text(Math.round(percent	));
+					}
+			});
+    
+    	}
+    
+    });
+
 });	

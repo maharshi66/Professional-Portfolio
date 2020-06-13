df1d1acccaca1744ace0c37bdaa7898d138fcffa
Maharshi Shah <maharshishah@mail.usf.edu>
2020-06-13 16:35:35 +0530

    Added preloader screen with loading gif

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$(window).on("load", function(){
+	$(".loader .inner").fadeOut(500, function(){
+		$(".loader").fadeOut(750);
+	});
+});
+
 $(document).ready(function() {
 	
 	$('#slides').superslides({
@@ -32,6 +38,7 @@ $(document).ready(function() {
 	    }
 	});
 
+	//Skills animation for counting to number
     var skillsTopOffset = $(".skillsSection").offset().top;	
     $(window).scroll(function()	{
     	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
@@ -51,5 +58,33 @@ $(document).ready(function() {
     	}
     
     });
+
+    $("#navigation li a").click(function(e){
+    	e.preventDefault();
+
+    	var targetElement = $(this).attr("href");
+    	var tarPosition = $(targetElement).offset().top;
+
+    	$("html, body").animate({scrollTop: tarPosition - 50}, "slow");
+    });
+    
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+    	var body = $("body");
+
+    	if($(window).scrollTop() >= navTop) {
+    		body.css("padding-top", nav.outerHeight() + "px");	
+    		body.addClass("fixedNav");
+    	}
+    	else{
+    		body.css("padding-top", 0);	
+    		body.removeClass("fixedNav");
+    	}
+    }
 
 });	
